12950d6ac162445f9e198170534b96e1dfbea9a9
Monika Brzica <brzicamona@gmail.com>
2021-07-09 10:19:31 +0200

    created rating view

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,26 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <rating />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import rating from './views/rating'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  mode: 'history',
+  name: 'app',
+  components: {
+    rating
   }
+}
+</script>
+
+<style>
+#app {
+  display: flex;
+  min-height: 100%;
+  min-width: 100%;
+  background-color: #1B1E24;
 }
 </style>
---
+++ src/views/rating.vue
@@ -0,0 +1,74 @@
+<template>
+
+    <div class="rating-wrapper">
+      <div v-if="!clicked">
+        <img class="like-img"
+          @click="change()"
+          src="../assets/positive-vote.svg">
+        <p>Rate our service!</p>
+      </div>
+      <div v-else>
+        <img class="check-img"
+          src="../assets/check.svg">
+        <p>{{this.message}}</p>
+      </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'rating',
+  data: function () {
+    return {
+      clicked: false
+    }
+  },
+  computed: {
+    message () {
+      return this.$store.getters.getSettings.thankYouMsg
+    },
+    timeout () {
+      return this.$store.getters.getSettings.msgTimeout
+    }
+  },
+  methods: {
+    change () {
+      this.clicked = !this.clicked
+      setTimeout(function () { this.clicked = !this.clicked }.bind(this), this.timeout * 1000)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+   .rating-wrapper {
+     margin: 0 auto;
+     height: 100vh;
+     width:70%;
+
+     .like-img {
+       display: block;
+       margin: 0 auto;
+       margin-top: 287px;
+       width: 154px;
+       height: 157px;
+     }
+
+     .check-img{
+       display: block;
+       margin: 0 auto;
+       margin-top: 477px;
+       width: 52px;
+       height: 51px;
+
+     }
+       p{
+         color: rgba(255,255,255, 0.7);
+         font-size: 32px;
+         text-align: center;
+         font-family: Roboto-Regular;
+       }
+     }
+</style>
