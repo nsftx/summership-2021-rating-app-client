d00823ee963f5f13e01e1a3acd3521e5e51426e4
Andric34 <ivanand34@gmail.com>
2021-07-09 15:54:03 +0200

    implemented emoticon component to rating

---
+++ src/component/emoticon.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="main" v-on:click="change()">
+    <div class="left-border"
+         :style="{ 'background-color': data.bgColor }">
+    </div>
+      <img :src="require('../assets/'+ this.data.imgSrc)"
+           alt="Emoticon"
+           :style="{ 'background-color': this.data.bgColor}">
+    </div>
+</template>
+<script>
+export default {
+  name: 'Emoticon',
+  props: {
+    data: Object
+  },
+  data: function () {
+    return {
+    }
+  },
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
+<style lang="scss" scoped>
+.main {
+  height: 141px;
+  .left-border{
+    height: 100%;
+    width: 5px;
+    display: inline-block;
+    margin-right: -5px;
+    border-top-left-radius: 5px;
+    border-bottom-left-radius: 5px;
+  }
+  img{
+    width: 64px;
+    height: 64px;
+    padding: 38.5px 62.5px;
+    border-radius: 5px;
+    opacity: 0.3;
+  }
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -9,8 +9,14 @@ export default new Vuex.Store({
       numOfEmotions: 3,
       thankYouMsg: 'Thank you for your rating!',
       msgTimeout: 5
-    }
-
+    },
+    emoticonsArr: [
+      { bgColor: 'rgba(0, 168, 107,1)', imgName: 'emoticon-outline.svg' },
+      { bgColor: 'rgba(65, 179, 233,1)', imgName: 'emoticon-happy-outline.svg' },
+      { bgColor: 'rgba(122, 122, 122,1)', imgName: 'emoticon-neutral-outline.svg' },
+      { bgColor: 'rgba(255, 186, 19,1)', imgName: 'emoticon-sad-outline.svg' },
+      { bgColor: 'rgba(249, 88, 90,1)', imgName: 'emoticon-angry-outline.svg' }
+    ]
   },
   mutations: {
   },
@@ -21,6 +27,9 @@ export default new Vuex.Store({
   getters: {
     getSettings (state) {
       return state.settings
+    },
+    getEmoticonsArr (state) {
+      return state.emoticonsArr
     }
   }
 })
