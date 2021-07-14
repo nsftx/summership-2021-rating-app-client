f81cf7936c5bd6e44b55a3381ad4d038b19a7df6
Monika Brzica <brzicamona@gmail.com>
2021-07-14 14:41:37 +0200

    change emoji images name

--- src/main.js
+++ src/main.js
@@ -2,8 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Emoticon from './component/emoticon'
-import Rating from './views/rating'
+
 Vue.config.productionTip = false
 
 new Vue({
@@ -12,14 +11,4 @@ new Vue({
   render: h => h(App)
 }).$mount('#app')
 
-export default {
-  install (Vue, options) {
-    if (!options || !options.store) {
-      throw new Error('Please initialise plugin with a Vuex store.')
-    }
-
-    options.store.registerModule('rating-app-client', store)
-
-    Vue.component('dummy-button', Emoticon, Rating)
-  }
-}
+export default Vue
