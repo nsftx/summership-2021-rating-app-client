b9f7cc32a50f5e2da5d0bef0e3636fabaa75ebee
Monika Brzica <brzicamona@gmail.com>
2021-07-19 14:58:48 +0200

    import rating-app-client repo

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 // vue event bus
 export const bus = new Vue()
 Vue.config.productionTip = false
-new Vue({
+
+const Public = new Vue({
   render: h => h(App)
 }).$mount('#app')
+
+export default {
+  Public
+}
